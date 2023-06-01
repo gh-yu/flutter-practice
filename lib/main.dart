f09aa66415a640dbfa86d0b61a1e938d3e12a447
gh-yu <yooumi99@gmail.com>
2023-06-01 09:30:34 +0900

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
 
 void main() {
   runApp(App()); // root Widget
@@ -22,14 +23,113 @@ class App extends StatelessWidget {
     // navigation bar, bottom bar, tob bar, center of screen ...
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          // actions: [],
-          // elevation: 99, // shadow
-          // centerTitle: true,
-          title: Text('Hello flutter!'),
+          // appBar: AppBar(
+          //   // actions: [],
+          //   // elevation: 99, // shadow
+          //   // centerTitle: true,
+          //   title: Text('Hello flutter!'),
+          // ),
+          // body: Center(
+          //   child: Text('Hello world!'),
+          // ),
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xFFF1B33B),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white),
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // button
+      decoration: BoxDecoration(
+        color: Color(0xFF1F2123),
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 45,
+          vertical: 17,
         ),
-        body: Center(
-          child: Text('Hello world!'),
+        child: Text(
+          'Transfer',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
         ),
       ),
     );
